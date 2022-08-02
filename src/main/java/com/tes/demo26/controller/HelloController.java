571c065beafb85aa498ac9f323673b432e3b3da2
zoutao98 <ezoutao@foxmail.com>
2022-08-02 12:01:08 +0800

    使用自动配置security

---
+++ src/main/java/com/tes/demo26/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.tes.demo26.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> index(){
+
+        return ResponseEntity.ok("index");
+    }
+    
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hello");
+    }
+}
